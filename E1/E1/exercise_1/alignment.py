07d49d2da6b65c84c02a1b01f24c9013411329ad
Bussler <maarten.bussler@gmail.com>
2022-05-10 00:15:21 +0200

    procrustes

--- E1/E1/exercise_1/alignment.py
+++ E1/E1/exercise_1/alignment.py
@@ -3,6 +3,13 @@ import numpy as np
 from pathlib import Path
 
 
+def computeMean(points):
+    mean = np.zeros(3)
+    for p in points:
+        mean += p
+    return mean / len(points)
+
+
 def procrustes_align(pc_x, pc_y):
     """
     calculate the rigid transform to go from point cloud pc_x to point cloud pc_y, assuming points are corresponding
@@ -19,6 +26,32 @@ def procrustes_align(pc_x, pc_y):
     # 3. estimate rotation
     # 4. estimate translation
     # R and t should now contain the rotation (shape 3x3) and translation (shape 3,)
+
+    xMean = computeMean(pc_x)
+    yMean = computeMean(pc_y)
+
+    X = np.zeros((3, len(pc_x)))
+    for i in range(0, len(pc_x)):
+        X[:, i] = pc_x[i] - xMean
+
+    Y = np.zeros((3, len(pc_y)))
+    for i in range(0, len(pc_y)):
+        Y[:, i] = pc_y[i] - yMean
+
+    U, s, V = np.linalg.svd(np.matmul(X, Y.transpose()), full_matrices=True)
+
+    rotation = np.matmul( V.transpose(), U)
+
+    SMatrix = np.identity(3)
+    if np.linalg.det(rotation) == -1:
+        SMatrix[2,2] = -1
+
+    rotation = np.matmul(np.matmul(V.transpose(), SMatrix), U.transpose())
+
+    translation = yMean - xMean
+    t = rotation.dot(translation) - rotation.dot(yMean) + yMean
+    R = rotation
+
     # TODO: Your implementation ends here ###############
 
     t_broadcast = np.broadcast_to(t[:, np.newaxis], (3, pc_x.shape[0]))
