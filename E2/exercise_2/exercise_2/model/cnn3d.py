010a9572e7829ea5cdab8d2f3e442ca5c49defdf
Philipp Eckart-Weissbaecker <philipp.eckart@tum.de>
2022-05-31 13:34:02 +0200

    Add parts for 3DCNN

--- E2/exercise_2/exercise_2/model/cnn3d.py
+++ E2/exercise_2/exercise_2/model/cnn3d.py
@@ -15,8 +15,14 @@ class MLPConv(nn.Module):
         super().__init__()
         # TODO: Define MLPConv model as nn.Sequential as described in the paper (Conv3d, ReLU, Conv3D, ReLU, Conv3D, ReLU)
         # The first conv has kernel_size and stride provided as the parameters, rest of the convs have 1x1x1 filters, with default stride
-        self.model = None
-
+        self.model = nn.Sequential(
+            nn.Conv3d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride),
+            nn.ReLU(),
+            nn.Conv3d(in_channels=out_channels, out_channels=out_channels, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv3d(in_channels=out_channels, out_channels=out_channels, kernel_size=1),
+            nn.ReLU()
+        )
     def forward(self, x):
         """
         :param x: tensor of shape [B, C, D, H, W], where B = batch size, C = num feature channels, D = depth of volume, H and W = height and width of volume
@@ -37,7 +43,7 @@ class ThreeDeeCNN(nn.Module):
         super().__init__()
 
         # TODO: Define backbone as sequence of 3 MLPConvs as per the paper
-        self.backbone = None
+        self.backbone = nn.Sequential(MLPConv(1, 48, 6, 2), MLPConv(48, 160, 5, 2), MLPConv(160, 512, 3, 2))
 
         self.feature_cube_side = 2  # side of resulting volume after last MLPConv layer
 
@@ -46,10 +52,11 @@ class ThreeDeeCNN(nn.Module):
         for i in range(8):
             self.partial_predictors.append(
                 # TODO: partial predictor linear layers as per the paper
+                nn.Sequential(nn.Linear(512, n_classes), nn.Softmax(dim=1))
             )
 
         # TODO: add predictor for full 2x2x2 feature volume
-        self.full_predictor = None
+        self.full_predictor = nn.Sequential(nn.Linear(4096,2048), nn.Linear(2048, n_classes), nn.Softmax(dim=1))
 
     def forward(self, x):
         """
@@ -59,7 +66,7 @@ class ThreeDeeCNN(nn.Module):
         batch_size = x.shape[0]
 
         # TODO: Get backbone features
-        backbone_features = None
+        backbone_features = self.backbone(x)
 
         predictions_partial = []
         # get prediction for each of the partial objects
@@ -69,11 +76,11 @@ class ThreeDeeCNN(nn.Module):
                     partial_predictor = self.partial_predictors[d * self.feature_cube_side ** 2 + h * self.feature_cube_side + w]
 
                     # TODO: get prediction for object for backbone feature at d, h, w
-                    partial_object_prediction = None
+                    partial_object_prediction = partial_predictor(backbone_features[:,:,d,h,w])
 
                     predictions_partial.append(partial_object_prediction)
 
         # TODO: Get prediction for whole object
-        full_prediction = None
+        full_prediction = self.full_predictor(backbone_features.view(batch_size, -1))
 
         return torch.stack([full_prediction] + predictions_partial, dim=1)
--- E2/exercise_2/exercise_2/training/train_3dcnn.py
+++ E2/exercise_2/exercise_2/training/train_3dcnn.py
@@ -60,12 +60,12 @@ def train(model, trainloader, valloader, device, config):
 
     # declare loss and move to specified device
     # TODO: Define loss
-    loss_criterion = None
+    loss_criterion = torch.nn.CrossEntropyLoss()
 
     loss_criterion.to(device)
 
     # TODO: declare optimizer (learning rate provided in config)
-    optimizer = None
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['learning_rate'])
 
     # set model to train, important if your network has e.g. dropout or batchnorm layers
     model.train()
@@ -82,18 +82,20 @@ def train(model, trainloader, valloader, device, config):
             ShapeNetVox.move_batch_to_device(batch, device)
 
             # TODO: zero out previously accumulated gradients
+            optimizer.zero_grad()
 
             # TODO: forward pass
-            prediction = None
+            prediction = model(batch['voxel'])
 
             # TODO: compute total loss = sum of loss for whole prediction + losses for partial predictions
             loss_total = torch.zeros([1], dtype=batch['voxel'].dtype, requires_grad=True).to(device)
             for output_idx in range(prediction.shape[1]):
-                loss_total = loss_total + None  # TODO: Loss due to prediction[:, output_idx, :] (output_idx=0 for global prediction, 1-8 local)
+                loss_total = loss_total + loss_criterion(prediction[:, output_idx, :], batch['label'])  # TODO: Loss due to prediction[:, output_idx, :] (output_idx=0 for global prediction, 1-8 local)
 
             # TODO: compute gradients on loss_total (backward pass)
-
+            loss_total.backward()
             # TODO: update network params
+            optimizer.step()
 
             # loss logging
             train_loss_running += loss_total.item()
@@ -117,12 +119,27 @@ def train(model, trainloader, valloader, device, config):
 
                     with torch.no_grad():
                         # TODO: Get prediction scores
-                        prediction = None
+                        prediction = model(batch_val['voxel'])
 
+                    print(prediction)
+                    pred_total = torch.zeros([1], dtype=batch['voxel'].dtype, requires_grad=True).to(device)
+                    loss_total_val = torch.zeros([1], dtype=batch['voxel'].dtype, requires_grad=True).to(device)
+                    for output_idx in range(prediction.shape[1]):
+                        pred_total = pred_total + prediction[:, output_idx, :], batch['label']
+                        loss_total_val += loss_total_val + loss_criterion(prediction[:, output_idx, :], batch['label'])
                     # TODO: Get predicted labels from scores
-                    predicted_label = None
+
+
+                    _, predicted_label = torch.max(prediction, dim=1)
+
 
                     # TODO: keep track of total / correct / loss_total_val
+                    total += predicted_label.shape[0]
+                    
+                    print(predicted_label.shape)
+                    correct += (predicted_label == batch['label']).sum().item()
+
+                    loss_total_val += loss_criterion(prediction, batch['label']).item()
 
                 accuracy = 100 * correct / total
 
