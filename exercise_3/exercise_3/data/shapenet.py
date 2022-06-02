c2c4821632da6d8ba811b51f0788231656068216
Bussler <maarten.bussler@gmail.com>
2022-06-02 23:47:35 +0200

    Initial push of data loader, somethings still wrong with data read in

--- exercise_3/exercise_3/data/shapenet.py
+++ exercise_3/exercise_3/data/shapenet.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 from pathlib import Path
 import json
 
@@ -26,8 +27,22 @@ class ShapeNet(torch.utils.data.Dataset):
         target_df = ShapeNet.get_shape_df(df_id)
 
         # TODO Apply truncation to sdf and df
+        input_sdf = np.where(input_sdf < -3, -3, input_sdf) # M TODO better way to do this?
+        input_sdf = np.where(input_sdf > 3, 3, input_sdf)
+
+        target_df = np.where(target_df < -3, -3, target_df)
+        target_df = np.where(target_df > 3, 3, target_df)
+
         # TODO Stack (distances, sdf sign) for the input sdf
-        # TODO Log-scale target df
+        signs = np.sign(input_sdf)
+        helperArray = np.empty((2,input_sdf.shape[0], input_sdf.shape[1], input_sdf.shape[2]), dtype= float)
+        helperArray[0] = input_sdf
+        helperArray[1] = signs
+
+        input_sdf = helperArray
+
+        # TODO Log-scale target df M TODO also scale prediction?
+        target_df = np.log(target_df+1)
 
         return {
             'name': f'{sdf_id}-{df_id}',
@@ -47,10 +62,38 @@ class ShapeNet(torch.utils.data.Dataset):
     def get_shape_sdf(shapenet_id):
         sdf = None
         # TODO implement sdf data loading
+        
+        category, fileName = shapenet_id.split('/')
+        fileName = fileName + '.sdf'
+
+        f = open(ShapeNet.dataset_sdf_path / category / fileName, "r")
+
+        dim = np.fromfile(f, np.uint64, 3)
+        numElements = dim[0]*dim[1]*dim[2]
+
+        sdf = np.fromfile(f, dtype = np.float32, count = numElements) # M TODO float32 correct data type here? offset rework!
+
+        sdf = np.reshape(sdf, (dim[0], dim[1], dim[2]))
+
+        f.close()
         return sdf
 
     @staticmethod
     def get_shape_df(shapenet_id):
         df = None
         # TODO implement df data loading
+
+        category, fileName = shapenet_id.split('/')
+        fileName = fileName + '.df'
+
+        f = open(ShapeNet.dataset_df_path / category / fileName, "r")
+
+        dim = np.fromfile(f, np.uint64, 3)
+        numElements = dim[0]*dim[1]*dim[2]
+
+        df = np.fromfile(f, dtype = np.float32, count = numElements) # M TODO float32 correct data type here?
+
+        df = np.reshape(df, (dim[0], dim[1], dim[2]))
+
+        f.close()
         return df
